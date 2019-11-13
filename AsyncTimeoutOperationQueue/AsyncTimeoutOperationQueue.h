22c052e36b5d4a85f9987044ea018f26a3a11baa
Sergey Khliustin <severehed@gmail.com>
2019-11-13 13:28:59 +0200

    updates

---
+++ AsyncTimeoutOperationQueue/AsyncTimeoutOperationQueue.h
@@ -0,0 +1,19 @@
+//
+//  AsyncTimeoutOperationQueue.h
+//  AsyncTimeoutOperationQueue
+//
+//  Created by Sergey Khliustin on 13.11.2019.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AsyncTimeoutOperationQueue.
+FOUNDATION_EXPORT double AsyncTimeoutOperationQueueVersionNumber;
+
+//! Project version string for AsyncTimeoutOperationQueue.
+FOUNDATION_EXPORT const unsigned char AsyncTimeoutOperationQueueVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AsyncTimeoutOperationQueue/PublicHeader.h>
+
+
